572eb9a9709a4dcf3d089eeed2b855966c98e2aa
Felipe-Mina <88908796+Felipe-Mina@users.noreply.github.com>
2022-10-13 14:40:08 -0300

    Promesas anidadas

--- 02-FUNDAMENTOS/promesas.js
+++ 02-FUNDAMENTOS/promesas.js
@@ -43,26 +43,38 @@ const getSalario = ( id ) => {
     })
 };
 
-const id = 4;
+const id = 10;
 
-/* getEmpleado(id)
-    .then( empleado => console.log( empleado ) )
-    .catch( err => console.log( err ) );
+/**
+ **   getEmpleado(id)
+ **       .then( empleado => console.log( empleado ) )
+ **       .catch( err => console.log( err ) );
 
-getSalario(id)
-    .then( salario => console.log( salario ) )
-    .catch( err => console.log( err ) ); */
+ **   getSalario(id)
+ **       .then( salario => console.log( salario ) )
+ **       .catch( err => console.log( err ) );
+
+
+ *?   getEmpleado( id )
+ *?       .then( empleado => {
+
+ *?           getSalario( id )
+ *?           .then( salario => {
+
+ *?               console.log( `El empleado ${empleado.nombre} tiene un salario de ${salario.salario}`)
+
+ *?           })
+
+ *?       })
+ *?       .catch( err => console.log( err ) )
+*/
+
+let nombre
 
 getEmpleado( id )
     .then( empleado => {
-
-        getSalario( id )
-         .then( salario => {
-
-            console.log( `El empleado ${empleado.nombre} tiene un salario de ${salario.salario}`)
-
-         })
-
+        nombre = empleado.nombre
+        return getSalario( id )
     })
+    .then( salario => console.log( `El empleado ${nombre} tiene un salario de ${salario.salario}` ) )
     .catch( err => console.log( err ) )
-
